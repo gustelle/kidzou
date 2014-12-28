0a453df283dd0359f1a2d3199b36ba59ff129dc7
Guillaume Patin <guillaume@kidzou.fr>
2014-12-28 08:33:07 +0100

    Amélioration du moteur de recherche pour ajouter la searchbox

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = 'customer-analytics';
+	const VERSION = 'searchbox';
 
 	/**
 	 * @TODO - Rename "plugin-name" to the name of your plugin
