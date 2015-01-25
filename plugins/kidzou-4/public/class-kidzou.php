56a68a7b91ebf598adaa16b16c216bf640d09bef
Guillaume Patin <guillaume@kidzou.fr>
2015-01-25 14:29:54 +0100

    fix probleme de conversion en recette / prod sur les floatvalues de latitude/longitude qui causaient une erreur de conversion à l'insertion en base

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = '0115-geods';
+	const VERSION = '0115-geods-fix2';
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
