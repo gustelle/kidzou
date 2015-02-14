2383ded655c013a9287763326ece7e137a0ef81e
Guillaume Patin <guillaume@kidzou.fr>
2015-02-14 18:25:30 +0100

    mise à jour du numéro de version

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = '0215-fix27';
+	const VERSION = '0215-fix28';
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
