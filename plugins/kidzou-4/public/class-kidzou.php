6787230080154ef12ce54c5555fafd80922b5b0c
Guillaume Patin <guillaume@kidzou.fr>
2015-02-25 08:22:55 +0100

    Préparation de la realse stcyp, correction d'un test sur array_search dans le traitement des events

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = '0215-fix32';
+	const VERSION = 'stcyp';
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
