810ab0a6fe08531771d5f1944b252e2dc4121e25
Guillaume Patin <guillaume@kidzou.fr>
2015-11-09 22:54:32 +0100

    Refonte de la fonction "A proximité"

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = 'API-V28';
+	const VERSION = 'API-V29';
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
