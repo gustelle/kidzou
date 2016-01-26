b32287c4e0d75c909a850caa324e5c5035887306
Guillaume Patin <guillaume@kidzou.fr>
2016-01-26 20:33:35 +0100

    <li>Correction d'un bug qui double les clients à la créatio d'un nouveau client depuis un post</li> 	<li>Correction d'un bug qui propose le lieu alors qu'il est déjà renseigné dans la saisie d'un event</li> 	<li>Possibilité d'éditer le client depuis un lien dans l'écran d'édition d'un post</li> 	<li>Affichage des posts du même client dans l'écran d'édition d'un post</li>

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = 'HernieDiscale-V2';
+	const VERSION = 'amad';
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
@@ -39,7 +39,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	public static $version_description = "Refactoring majeur des classes, ajout d'une Extension Chrome pour importer les contenus";
+	public static $version_description = "Améliorations mineures des écrans d'admin";
 
 	/**
 	 * @TODO - Rename "plugin-name" to the name of your plugin
