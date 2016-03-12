154792181222eb7acad3fef50b6c5665644e3674
Guillaume Patin <guillaume@kidzou.fr>
2016-03-12 20:46:01 +0100

    fix sur le single vote suite passage react

--- plugins/kidzou-4/public/class-kidzou.php
+++ plugins/kidzou-4/public/class-kidzou.php
@@ -30,7 +30,7 @@ class Kidzou {
 	 *
 	 * @var     string
 	 */
-	const VERSION = 'Fourty+f4';
+	const VERSION = 'Fourty+f5';
 
 	/**
 	 * Plugin version, used for cache-busting of style and script file references.
